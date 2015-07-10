7ec69e0369d5662d8dd4b5992d12f7d58dfd5376
krujos <jkruck@pivotal.io>
2015-07-10 15:56:12 -0600

    Stub in plugin methods

--- usagereport.go
+++ usagereport.go
@@ -1,10 +1,44 @@
 package main
 
-import "fmt"
+import "github.com/cloudfoundry/cli/plugin"
 
+//UsageReportCmd the plugin
 type UsageReportCmd struct {
 }
 
+//GetMetadata returns metatada
+func (cmd *UsageReportCmd) GetMetadata() plugin.PluginMetadata {
+	return plugin.PluginMetadata{
+		Name: "usage-report",
+		Version: plugin.VersionType{
+			Major: 1,
+			Minor: 0,
+			Build: 4,
+		},
+		Commands: []plugin.Command{
+			{
+				Name:     "usage-report",
+				HelpText: "Report AI and memory usage for orgs and spaces",
+				UsageDetails: plugin.Usage{
+					Usage: "cf usage-report",
+				},
+			},
+		},
+	}
+}
+
+//UsageReportCommand doer
+func (cmd *UsageReportCmd) UsageReportCommand(cli plugin.CliConnection, args []string) {
+	//Do the things
+}
+
+//Run runs the plugin
+func (cmd *UsageReportCmd) Run(cliConnection plugin.CliConnection, args []string) {
+	if args[0] == "usage-report" {
+		cmd.UsageReportCommand(cliConnection, args)
+	}
+}
+
 func main() {
-	fmt.Println("Hello")
+	plugin.Start(new(UsageReportCmd))
 }
