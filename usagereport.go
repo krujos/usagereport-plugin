181895c49cd950a957c5c55b18aa086eae40fd57
krujos <jkruck@pivotal.io>
2015-07-10 22:06:51 -0500

    Move apihelper to it's own files

--- usagereport.go
+++ usagereport.go
@@ -1,11 +1,6 @@
 package main
 
-import (
-	"errors"
-
-	"github.com/cloudfoundry/cli/plugin"
-	"github.com/krujos/cfcurl"
-)
+import "github.com/cloudfoundry/cli/plugin"
 
 //UsageReportCmd the plugin
 type UsageReportCmd struct {
@@ -17,9 +12,6 @@ type organization struct {
 	quotaURL  string
 	spacesURL string
 }
-
-//APIHelper to wrap cf curl results
-type APIHelper struct{}
 
 //GetMetadata returns metatada
 func (cmd *UsageReportCmd) GetMetadata() plugin.PluginMetadata {
@@ -52,45 +44,6 @@ func (cmd *UsageReportCmd) Run(cli plugin.CliConnection, args []string) {
 	if args[0] == "usage-report" {
 		cmd.UsageReportCommand(cli, args)
 	}
-}
-
-func (api *APIHelper) getOrgs(cli plugin.CliConnection) ([]organization, error) {
-	orgsJSON, err := cfcurl.Curl(cli, "/v2/organizations")
-
-	if nil != err {
-		//TODO Swollow this?
-		return nil, errors.New("Failed to get orgs!")
-	}
-	orgs := []organization{}
-	for _, o := range orgsJSON["resources"].([]interface{}) {
-		theOrg := o.(map[string]interface{})
-		entity := theOrg["entity"].(map[string]interface{})
-		metadata := theOrg["metadata"].(map[string]interface{})
-		orgs = append(orgs,
-			organization{
-				name:      entity["name"].(string),
-				url:       metadata["url"].(string),
-				quotaURL:  entity["quota_definition_url"].(string),
-				spacesURL: entity["spaces_url"].(string),
-			})
-	}
-	return orgs, nil
-}
-
-func (api *APIHelper) getQuotaMemoryLimit(cli plugin.CliConnection, quotaURL string) (float64, error) {
-	quotaJSON, err := cfcurl.Curl(cli, quotaURL)
-	if nil != err {
-		return 0, err
-	}
-	return quotaJSON["entity"].(map[string]interface{})["memory_limit"].(float64), nil
-}
-
-func (api *APIHelper) getOrgMemoryUsage(cli plugin.CliConnection, org organization) (float64, error) {
-	usageJSON, err := cfcurl.Curl(cli, org.url+"/memory_usage")
-	if nil != err {
-		return 0, err
-	}
-	return usageJSON["memory_usage_in_mb"].(float64), nil
 }
 
 func main() {
