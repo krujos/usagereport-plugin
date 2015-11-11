e4e366632ef50f749d281f0a1b7e43e460e91e08
krujos <jkruck@pivotal.io>
2015-11-11 15:39:25 -0500

    update godeps

---
+++ Godeps/_workspace/src/github.com/krujos/cfcurl/cfcurl_suite_test.go
@@ -0,0 +1,13 @@
+package cfcurl_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestCfcurl(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Cfcurl Suite")
+}
